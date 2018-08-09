3e848e1d76b2fc6943276ff7eeadb101e2de0e3c
Rijuvan.ansari <rijuvanansari@yahoo.com>
2018-08-09 11:14:28 +0530

    .

---
+++ config-clientApp/src/main/java/com/soft/infg/configclientApp/ConfigClientAppApplication.java
@@ -0,0 +1,38 @@
+package com.soft.infg.configclientApp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@EnableDiscoveryClient
+@SpringBootApplication
+@RestController
+//@RefreshScope
+public class ConfigClientAppApplication {
+
+	@Autowired
+	private ConfigClientAppConfiguration properties;
+	
+	@Value("${some.other.property}")
+		private String someOtherProerty;
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientAppApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String printConfig()
+	{
+		
+		StringBuilder sb=new StringBuilder();
+		sb.append(properties.getProperty());
+		sb.append("||");
+		sb.append(someOtherProerty);
+		return sb.toString();
+		
+	}
+}
